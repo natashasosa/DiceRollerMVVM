e2fe4a25ef0d1d1611370e86b930639c09814406
natashasosa <natasha.sosa@poatek.com>
2023-11-28 10:54:39 -0300

    IntroduceModels and start ContentViewViewModel file

---
+++ DiceRollerMVVM/View/ContentViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ContentViewViewModel.swift
+//  DiceRollerMVVM
+//
+//  Created by Natasha Rebelo on 10/11/23.
+//
+
+import Foundation
+
+extension ContentView {
+    
+    @MainActor
+    class ViewModel: ObservableObject {
+
+    }
+}
